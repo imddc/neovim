9fa0994db4d1d9d306bae80e93439acaa527b83f
Du Cheng <easyhoo2426@163.com>
2024-03-06 20:06:20 +0800

    feat: add os

--- init.lua
+++ init.lua
@@ -13,3 +13,18 @@ require('plugins.plg-lsp')
 require('plugins.bufferline')
 require('plugins.gitsigns')
 require('plugins.telescope')
+
+local has = function(x)
+  return vim.fn.has(x) == 1
+end
+
+local is_mac = has 'macunix'
+local is_win = has 'win32'
+
+if is_mac then
+  require('macos')
+end
+
+if is_win then
+  return('windows')
+end
--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,48 +3,26 @@ vim.g.mapleader = ' '
 -- define common options
 local keymap = vim.keymap
 
------------------
--- Normal mode --
------------------
+keymap.set('n', 'x', '"_x')
+keymap.set('n', 'dw', 'vb"_d')
+
 keymap.set('i', 'jk', '<ESC>')
 keymap.set('i', 'jj', '<ESC>')
 keymap.set('i', 'jk', '<ESC>')
 keymap.set('i', 'kk', '<ESC>')
 
--- Hint: see `:h vim.map.set()`
--- Better window navigation
 keymap.set('n', '<C-h>', '<C-w>h' )
 keymap.set('n', '<C-j>', '<C-w>j' )
 keymap.set('n', '<C-k>', '<C-w>k' )
 keymap.set('n', '<C-l>', '<C-w>l' )
 keymap.set('n', '<S-h>', '0' )
 keymap.set('n', '<S-l>', '$' )
-
--- Resize with arrows
--- delta: 2 lines
-keymap.set('n', '<C-Up>', ':resize -2<CR>' )
-keymap.set('n', '<C-Down>', ':resize +2<CR>')
-keymap.set('n', '<C-Left>', ':vertical resize -2<CR>')
-keymap.set('n', '<C-Right>', ':vertical resize +2<CR>')
-
--- 窗口
 keymap.set('n', '<leader>sv', "<C-w>v")
 keymap.set('n', '<leader>sh', "<C-w>s")
-
--- 取消高亮
 keymap.set('n', '<leader>nh', ":nohl<CR>")
-
--- nvim-tree
 keymap.set('n', '<leader>;', ':Neotree toggle<CR>')
-
-
--- 切换buffer
 keymap.set('n', '<C-L>', ':bnext<CR>')
 keymap.set('n', '<C-H>', ':bprevious<CR>')
-
------------------
--- Visual mode --
------------------
 
 keymap.set('v', '<', '<gv')
 keymap.set('v', '>', '>gv')
