b79eb1e5f5d6c382d616f87c7a98b79221fb1e37
Du Cheng <easyhoo2426@163.com>
2024-03-14 15:12:30 +0800

    feat: merge config

---
+++ lua/keymap.lua
@@ -0,0 +1,29 @@
+vim.g.mapleader = ' '
+local keymap = vim.keymap
+
+keymap.set('n', '<c-a>', 'ggVG')
+
+keymap.set({ 'n', 'x' }, '<leader>p', '"0p')
+
+keymap.set('n', '<leader>q', '<cmd>q<cr>')
+keymap.set('n', '<leader>w', '<cmd>w<cr>')
+keymap.set('n', '<leader>x', '<cmd>x<cr>')
+
+keymap.set('n', 'j', [[v:count?'j':'gj']], { noremap = true, expr = true })
+keymap.set('n', 'k', [[v:count?'k':'gk']], { noremap = true, expr = true })
+
+keymap.set('n', '<leader>nh', ':nohl<CR>', { desc = 'Clear search highlights' })
+
+-- esc
+keymap.set('i', 'jk', '<ESC>')
+keymap.set('i', 'jj', '<ESC>')
+keymap.set('i', 'jk', '<ESC>')
+keymap.set('i', 'kk', '<ESC>')
+
+-- 行首行尾
+keymap.set('n', '<S-h>', '0')
+keymap.set('n', '<S-l>', '$')
+
+-- 切换buffer
+keymap.set('n', '<c-h>', ':BufferLineCycleNext<cr>')
+keymap.set('n', '<c-l>', ':BufferLineCyclePrev<cr>')
