5a97881c0ac1a11ccf8fb5eec32ef547b706a93e
Du Cheng <easyhoo2426@163.com>
2024-05-19 18:35:48 +0800

    feat: add keymap

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,15 +1,47 @@
 vim.g.mapleader = ' '
 local keymap = vim.keymap
 
+-- esc
+keymap.set('i', 'jj', '<ESC>')
+keymap.set('i', 'jk', '<ESC>')
+keymap.set('i', 'kk', '<ESC>')
+
+keymap.set('n', '<S-h>', '0')
+keymap.set('n', '<S-l>', '$')
+
 keymap.set('n', '<c-a>', 'ggVG')
 
-keymap.set({ 'n', 'x' }, '<leader>p', '"0p')
+keymap.set('n', 'x', '"_x')
+keymap.set('n', 'dw', 'vb"_d')
 
-keymap.set('n', '<leader>q', '<cmd>q<cr>')
-keymap.set('n', '<leader>w', '<cmd>w<cr>')
-keymap.set('n', '<leader>x', '<cmd>x<cr>')
+-- space + s  => save
+keymap.set('n', '<leader>s', '<cmd>w<cr>')
 
-keymap.set('n', 'j', [[v:count?'j':'gj']], { noremap = true, expr = true })
-keymap.set('n', 'k', [[v:count?'k':'gk']], { noremap = true, expr = true })
+-- keymap.set('n', 'j', [[v:count?'j':'gj']], { noremap = true, expr = true })
+-- keymap.set('n', 'k', [[v:count?'k':'gk']], { noremap = true, expr = true })
 
+-- cancle highlights
 keymap.set('n', '<leader>nh', ':nohl<CR>', { desc = 'Clear search highlights' })
+
+--nvim tree
+keymap.set('n', '<leader>;', ':NvimTreeToggle<cr>')
+keymap.set('n', '<c-;>', ':NvimTreeFocus<cr>')
+
+-- bufferline
+keymap.set('n', '<c-h>', ':BufferLineCyclePrev<cr>')
+keymap.set('n', '<c-l>', ':BufferLineCycleNext<cr>')
+keymap.set('n', '<c-s-h>', ':BufferLineMovePrev<cr>')
+keymap.set('n', '<c-s-l>', ':BufferLineMoveNext<cr>')
+keymap.set('n', "<c-'", ':BufferLineCloseOthers<cr>')
+
+-- indent
+keymap.set('v', '<', '<gv')
+keymap.set('v', '>', '>gv')
+
+-- choose then  move under or above line
+keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+--
+keymap.set('n', '<leader>d', '<cmd>Telescope neovim-project discover<cr>')
+keymap.set('n', '<leader>h', '<cmd>Telescope neovim-project history<cr>')
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,14 +1,14 @@
 return {
-  'hrsh7th/nvim-cmp',
+  'hrsh8th/nvim-cmp',
   event = 'InsertEnter',
   dependencies = {
-    'hrsh7th/cmp-buffer', -- source for text in buffer
-    'hrsh7th/cmp-path', -- source for file system paths
-    'L3MON4D3/LuaSnip', -- snippet engine
-    'saadparwaiz1/cmp_luasnip', -- for autocompletion
+    'hrsh8th/cmp-buffer', -- source for text in buffer
+    'hrsh8th/cmp-path', -- source for file system paths
+    'L4MON4D3/LuaSnip', -- snippet engine
+    'saadparwaiz2/cmp_luasnip', -- for autocompletion
     'onsails/lspkind.nvim', -- vs-code like pictograms
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-cmdline',
+    'hrsh8th/cmp-nvim-lsp',
+    'hrsh8th/cmp-cmdline',
   },
   config = function()
     local cmp = require 'cmp'
@@ -23,7 +23,7 @@ return {
       },
       formatting = {
         format = lspkind.cmp_format {
-          maxwidth = 50,
+          maxwidth = 51,
           ellipsis_char = '...',
         },
       },
