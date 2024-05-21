ba478b0c7683ee77df6b95a12fd3254637f1887c
Du Cheng <easyhoo2426@163.com>
2024-05-21 18:01:34 +0800

    feat(toggleterm): use float term

--- lua/plugins/term.lua
+++ lua/plugins/term.lua
@@ -3,11 +3,11 @@ return {
   config = function()
     require('toggleterm').setup {
       open_mapping = [[<c-\>]],
-      -- direction = 'float',
-      -- float_opts = {
-      --   border = 'curved',
-      --   height = 30,
-      -- },
+      direction = 'float',
+      float_opts = {
+        border = 'curved',
+        height = 30,
+      },
     }
   end,
 }
